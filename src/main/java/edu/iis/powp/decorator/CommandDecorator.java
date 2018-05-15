0a32f23c9a1d086f7093d9e9a2a745a5d6604bc7
Kamil Krupa <kamilkrupa96@gmail.com>
2018-05-15 14:48:33 +0200

    Add abstraction layer for future commands decorators

---
+++ src/main/java/edu/iis/powp/decorator/CommandDecorator.java
@@ -0,0 +1,27 @@
+package edu.iis.powp.decorator;
+
+import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.adapter.InterceptCoordinatesAdapterPlotterDriver;
+import edu.iis.powp.command.CompoundCommand;
+import edu.iis.powp.command.IPlotterCommand;
+import edu.iis.powp.model.PlotterMovementModel;
+
+import java.util.List;
+
+public abstract class CommandDecorator implements IPlotterCommand {
+    protected CompoundCommand compoundCommand;
+    private List<PlotterMovementModel> coordinates;
+
+    public CommandDecorator(IPlotterCommand baseCommand) {
+        InterceptCoordinatesAdapterPlotterDriver plotterDriver = new InterceptCoordinatesAdapterPlotterDriver();
+        baseCommand.execute(plotterDriver);
+        coordinates = plotterDriver.getCoordinates();
+    }
+
+    @Override
+    public void execute(IPlotter plotter) {
+        compoundCommand = constructTransformedCompoundCommand(coordinates);
+    }
+
+    protected abstract CompoundCommand constructTransformedCompoundCommand(List<PlotterMovementModel> coordinates);
+}
