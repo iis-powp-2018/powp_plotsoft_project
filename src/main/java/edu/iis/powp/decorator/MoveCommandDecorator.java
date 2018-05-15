96c64b8b9f075a34e7eee8579bb4ed98978bfa17
Kamil Krupa <kamilkrupa96@gmail.com>
2018-05-15 14:48:53 +0200

    Implement Move command

---
+++ src/main/java/edu/iis/powp/decorator/MoveCommandDecorator.java
@@ -0,0 +1,44 @@
+package edu.iis.powp.decorator;
+
+import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.command.CompoundCommand;
+import edu.iis.powp.command.DrawToCommand;
+import edu.iis.powp.command.IPlotterCommand;
+import edu.iis.powp.command.SetPositionCommand;
+import edu.iis.powp.model.PlotterMovementModel;
+
+import java.util.List;
+
+public class MoveCommandDecorator extends CommandDecorator {
+    private int moveX;
+    private int moveY;
+
+    public MoveCommandDecorator(IPlotterCommand baseCommand, int moveX, int moveY) {
+        super(baseCommand);
+        this.moveX = moveX;
+        this.moveY = moveY;
+    }
+
+    @Override
+    protected CompoundCommand constructTransformedCompoundCommand(List<PlotterMovementModel> coordinates) {
+        CompoundCommand result = new CompoundCommand();
+        coordinates.forEach(coordinate -> {
+            IPlotterCommand command;
+            coordinate.setPosX(coordinate.getPosX() + moveX);
+            coordinate.setPosY(coordinate.getPosY() + moveY);
+            if(coordinate.isDrawing()) {
+                command = new DrawToCommand(coordinate.getPosX(), coordinate.getPosY());
+            } else {
+                command = new SetPositionCommand(coordinate.getPosX(), coordinate.getPosY());
+            }
+            result.addPlotterCommand(command);
+        });
+        return result;
+    }
+
+    @Override
+    public void execute(IPlotter plotter) {
+        super.execute(plotter);
+        compoundCommand.execute(plotter);
+    }
+}
