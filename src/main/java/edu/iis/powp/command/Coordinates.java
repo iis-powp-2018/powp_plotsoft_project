86076577951e3d74523bb4b9fbe5e063cbf685a7
adinowi <adinowi@gmail.com>
2018-05-27 22:26:09 +0200

    override equals and hashCode method

--- src/main/java/edu/iis/powp/command/Coordinates.java
+++ src/main/java/edu/iis/powp/command/Coordinates.java
@@ -15,6 +15,33 @@ public class Coordinates {
 	public int getPosY() {
 		return posY;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + posX;
+		result = prime * result + posY;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coordinates other = (Coordinates) obj;
+		if (posX != other.posX)
+			return false;
+		if (posY != other.posY)
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 }
