453b6b2e3e522f469466e06e5688e6836799b5f4
adinowi <adinowi@gmail.com>
2018-05-27 22:19:13 +0200

    added cooridnates class

---
+++ src/main/java/edu/iis/powp/command/Coordinates.java
@@ -0,0 +1,20 @@
+package edu.iis.powp.command;
+
+public class Coordinates {
+	private int posX, posY;
+	
+	public Coordinates(int posX, int posY) {
+		this.posX = posX;
+		this.posY = posY;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+	
+	
+}
