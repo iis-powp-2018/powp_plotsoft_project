77c34232dc59fb45fd8d26fe5e4092fd6122b130
adinowi <adinowi@gmail.com>
2018-05-28 16:02:13 +0200

    added javadoc for SimpleCommandFactory

--- src/main/java/edu/iis/powp/command/factory/SimpleCommandFactory.java
+++ src/main/java/edu/iis/powp/command/factory/SimpleCommandFactory.java
@@ -6,9 +6,17 @@ import java.util.Iterator;
 import edu.iis.powp.command.Coordinates;
 import edu.iis.powp.command.IPlotterCommand;
 
+/**
+ * Abstract factory for commands containing coordinates 
+ */
 public abstract class SimpleCommandFactory {
 	private static HashSet<Coordinates> coordinatesPool = new HashSet<>();
 	
+	/**
+	 *  Checking if passed coordinates are in coordinates pool, if not they are add to pool
+	 * @param coordinates Coordinates to check
+	 * @return pointer to coordinates from pool
+	 */
 	protected Coordinates checkPool(Coordinates coordinates) {
 		Coordinates buff = null;
 		
@@ -28,6 +36,11 @@ public abstract class SimpleCommandFactory {
 			
 	}
 	
+	/**
+	 * Create IPlotterCommand using coordinates from pool
+	 * @param coordinates which we want to have in returning object
+	 * @return IPlotterCommand using coordinates form pool
+	 */
 	public abstract IPlotterCommand makeSimpleCommand(Coordinates coordinates);
 	
 }
