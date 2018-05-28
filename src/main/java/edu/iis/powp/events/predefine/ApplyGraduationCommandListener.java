db974502f5e67a5ca9daba11843e06500fbe12ce
[Roksana_Traczyk] <[zakrencona15@wp.pl]>
2018-05-28 21:18:18 +0200

    Create graduation command listener

---
+++ src/main/java/edu/iis/powp/events/predefine/ApplyGraduationCommandListener.java
@@ -0,0 +1,35 @@
+package edu.iis.powp.events.predefine;
+
+import edu.iis.powp.command.IPlotterCommand;
+import edu.iis.powp.command.manager.PlotterCommandManager;
+import edu.iis.powp.decorator.GraduationCommandDecorator;
+import edu.iis.powp.features.CommandsFeature;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ApplyGraduationCommandListener implements ActionListener {
+    private JSpinner graduationSpinner;
+
+    public ApplyGraduationCommandListener(JSpinner graduationSpinner) {
+        this.graduationSpinner = graduationSpinner;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        PlotterCommandManager commandManager = CommandsFeature.getPlotterCommandManager();
+        IPlotterCommand currentCommand = commandManager.getCurrentCommand();
+        int graduation;
+        try {
+            graduation = (Integer) graduationSpinner.getValue();
+        } catch (ClassCastException f) {
+            return; // TODO: Implement error logging
+        }
+        if (currentCommand != null) {
+            GraduationCommandDecorator newCommand = new GraduationCommandDecorator(currentCommand, graduation);
+            commandManager.setCurrentCommand(newCommand);
+            graduationSpinner.setValue(0);
+        }
+    }
+}
