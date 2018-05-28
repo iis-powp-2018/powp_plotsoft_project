f9a822348f3776676f66801f7f8131eb055d558d
[Roksana_Traczyk] <[zakrencona15@wp.pl]>
2018-05-28 21:19:47 +0200

    Create run command listener

---
+++ src/main/java/edu/iis/powp/events/predefine/RunCommandListener.java
@@ -0,0 +1,26 @@
+package edu.iis.powp.events.predefine;
+
+import edu.iis.powp.app.DriverManager;
+import edu.iis.powp.command.IPlotterCommand;
+import edu.iis.powp.command.manager.PlotterCommandManager;
+import edu.iis.powp.features.CommandsFeature;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RunCommandListener implements ActionListener {
+    private DriverManager driverManager;
+
+    public RunCommandListener(DriverManager driverManager) {
+        this.driverManager = driverManager;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        PlotterCommandManager commandManager = CommandsFeature.getPlotterCommandManager();
+        IPlotterCommand currentCommand = commandManager.getCurrentCommand();
+        if (currentCommand != null) {
+            currentCommand.execute(driverManager.getCurrentPlotter());
+        }
+    }
+}
