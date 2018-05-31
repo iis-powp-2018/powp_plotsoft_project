d49a7eb9fd0f6a225e3368961210f3615c01a545
Kamil Prus <kami230596@gmail.com>
2018-05-31 17:04:17 +0200

    Add JavaDoc to CommandTransformer

--- src/test/java/edu/iis/powp/commandtransformer/CommandTransformer.java
+++ src/test/java/edu/iis/powp/commandtransformer/CommandTransformer.java
@@ -4,15 +4,27 @@ import edu.iis.powp.app.Application;
 import edu.iis.powp.decorator.gui.CommandTransformerCreatorWindow;
 import edu.iis.powp.decorator.gui.components.*;
 
+/**
+ * This class is used to setup CommandTransformerCreatorWindow
+ */
 public class CommandTransformer {
+
     private static CommandTransformerCreatorWindow commandTransformerCreatorWindow;
 
+    /**
+     * This method adds CommandTransformerCreatorWindow to Application passed as parameter
+     *
+     * @param application the application which CommandTransformerCreatorWindow will be attached
+     */
     public static void setupCommandTransformer(Application application) {
         commandTransformerCreatorWindow = new CommandTransformerCreatorWindow(application.getDriverManager());
         setupBasicWindowComponents();
         application.addWindowComponent("Command Transformer Creator", commandTransformerCreatorWindow);
     }
 
+    /**
+     * This method adds components to commandTransformerCreatorWindow
+     */
     private static void setupBasicWindowComponents() {
         commandTransformerCreatorWindow.addComponent("Flip", new FlipCommandComponent());
         commandTransformerCreatorWindow.addComponent("Move", new MoveCommandComponent());
@@ -21,6 +33,9 @@ public class CommandTransformer {
         commandTransformerCreatorWindow.addComponent("Rotate", new RotationCommandComponent());
     }
 
+    /**
+     * @return CommandTransformerCreatorWindow
+     */
     public static CommandTransformerCreatorWindow getCommandTransformerCreatorWindow() {
         return commandTransformerCreatorWindow;
     }
