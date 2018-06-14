f9def25d9ad6b3f5ca1aa2127d996425ecb946ff
Mrucznik <mrucznix@gmail.com>
2018-06-15 01:25:34 +0200

    Serializacja - złożone komendy

--- src/main/java/powp/InterfaceAdapter.java
+++ src/main/java/powp/InterfaceAdapter.java
@@ -1,31 +1,64 @@
 package powp;
 
 import com.google.gson.*;
+import edu.iis.powp.command.IPlotterCommand;
+import powp.commandManager.ICommand;
 
 import java.lang.reflect.Type;
+import java.util.Map;
 
 public class InterfaceAdapter<T> implements JsonSerializer<T>, JsonDeserializer<T> {
 
     private static final String CLASSNAME = "CLASSNAME";
     private static final String DATA = "DATA";
 
+    private int polymorphic = 1;
+
     public T deserialize(JsonElement jsonElement, Type type,
                          JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
 
-        JsonObject jsonObject = jsonElement.getAsJsonObject();
-        JsonPrimitive prim = (JsonPrimitive) jsonObject.get(CLASSNAME);
-        String className = prim.getAsString();
-        Class klass = getObjectClass(className);
-        Gson gson = new GsonBuilder().create();
-        return gson.fromJson(jsonObject.get(DATA), (Type)klass);
+        if(polymorphic == 1) {
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            JsonPrimitive prim = (JsonPrimitive) jsonObject.get(CLASSNAME);
+            String className = prim.getAsString();
+            Class klass = getObjectClass(className);
+            polymorphic = 0;
+            JsonElement element = jsonObject.get(DATA);
+            return jsonDeserializationContext.deserialize(element, klass);
+        }
+        else
+        {
+            Gson gson = new GsonBuilder()
+                    .enableComplexMapKeySerialization()
+                    .setPrettyPrinting()
+                    .registerTypeAdapter(IPlotterCommand.class, this)
+                    .create();
+            polymorphic = 1;
+            return gson.fromJson(jsonElement, type);
+        }
     }
-    public JsonElement serialize(T jsonElement, Type type, JsonSerializationContext jsonSerializationContext) {
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty(CLASSNAME, jsonElement.getClass().getName());
 
-        Gson gson = new GsonBuilder().create();
-        jsonObject.add(DATA, gson.toJsonTree(jsonElement));
-        return jsonObject;
+    public JsonElement serialize(T jsonElement, Type type, JsonSerializationContext jsonSerializationContext) {
+        if(polymorphic >= 1)
+        {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty(CLASSNAME, jsonElement.getClass().getName());
+
+            polymorphic--;
+            jsonObject.add(DATA, jsonSerializationContext.serialize(jsonElement));
+            return jsonObject;
+        }
+        else
+        {
+            Gson gson = new GsonBuilder()
+                    .enableComplexMapKeySerialization()
+                    .setPrettyPrinting()
+                    .registerTypeAdapter(IPlotterCommand.class, this)
+                    .create();
+            polymorphic = 100; //TODO: Zamiast 100 dać na ilość potrzebnych zagnieżdżonych serializacji
+            JsonElement je = gson.toJsonTree(jsonElement);
+            return je;
+        }
     }
     /****** Helper method to get the className of the object to be deserialized *****/
     private Class getObjectClass(String className) {
--- src/test/java/edu/iis/powp/command/CommandSerializerTest.java
+++ src/test/java/edu/iis/powp/command/CommandSerializerTest.java
@@ -6,10 +6,36 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandSerializerTest {
+
+    private class CompoundCommandTest implements ICompoundCommand {
+        private List<IPlotterCommand> commands = new ArrayList<>();
+
+        public CompoundCommandTest() {
+            commands.add(new DrawToCommand(0, 1));
+            commands.add(new DrawToCommand(1, 2));
+            commands.add(new DrawToCommand(2, 3));
+            commands.add(new DrawToCommand(4, 5));
+        }
+
+        @Override
+        public Iterator<IPlotterCommand> iterator() {
+            return commands.iterator();
+        }
+
+        @Override
+        public void execute(IPlotter plotter) {
+            for (IPlotterCommand command : commands) {
+                command.execute(plotter);
+            }
+        }
+    }
 
     @Test
     void MainTest()
@@ -33,4 +59,25 @@ class CommandSerializerTest {
         });
     }
 
+
+    @Test
+    void CompoundCommandTest()
+    {
+        IPlotterCommand compoundCommand = new CompoundCommandTest();
+        CommandSerializer commandSerializer = new CommandSerializer();
+        commandSerializer.exportCommand(compoundCommand, "compound_command.json");
+        Assert.assertEquals(true, Files.exists(Paths.get(("compound_command.json"))));
+
+        IPlotterCommand importingCommand = commandSerializer.importCommand("compound_command.json");
+        importingCommand.execute(new IPlotter() {
+            @Override
+            public void setPosition(int i, int i1) {
+            }
+
+            @Override
+            public void drawTo(int x, int y) {
+                Assert.assertEquals(x+1, y);
+            }
+        });
+    }
 }
