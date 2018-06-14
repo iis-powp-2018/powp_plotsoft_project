9bb49f2b96946cee84768369dea221468b516176
Rafikus <Rafikus95@gmail.com>
2018-06-14 22:53:10 +0200

    Adding and Removing strings from new command list

--- src/main/java/edu/iis/powp/command/gui/CommandEditorWindow.java
+++ src/main/java/edu/iis/powp/command/gui/CommandEditorWindow.java
@@ -3,21 +3,44 @@ package edu.iis.powp.command.gui;
 import edu.iis.powp.app.gui.WindowComponent;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CommandEditorWindow extends JFrame implements WindowComponent{
 
     private JPanel mainPanel;
-    private JList list1;
-    private JList list2;
+    private JList basicCommandsList;
+    private JList newCommandList;
     private JButton addButton;
     private JButton removeButton;
     private JTextField textField1;
-    private JButton button1;
-    private JButton button2;
+    private JButton saveButton;
+    private JButton clearButton;
+    DefaultListModel<String> basicCommands;
+    DefaultListModel<String> newCommands;
+
 
     public CommandEditorWindow(){
         this.setSize(600, 400);
         setContentPane(mainPanel);
+        basicCommands = new DefaultListModel<>();
+        basicCommandsList.setModel(basicCommands);
+
+        for(int i = 0; i < 15; i++){
+            basicCommands.addElement(String.valueOf(i));
+        }
+
+        newCommands = new DefaultListModel<>();
+        newCommandList.setModel(newCommands);
+
+
+        addButton.addActionListener(e -> {
+            newCommands.addElement(basicCommands.get(basicCommandsList.getSelectedIndex()));
+        });
+
+        removeButton.addActionListener(e -> {
+            newCommands.remove(newCommandList.getSelectedIndex());
+        });
     }
 
     @Override
@@ -28,4 +51,5 @@ public class CommandEditorWindow extends JFrame implements WindowComponent{
             this.setVisible(true);
         }
     }
+
 }
