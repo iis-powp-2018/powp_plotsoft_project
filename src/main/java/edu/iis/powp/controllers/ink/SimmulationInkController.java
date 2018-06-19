19983f92768f677d81a568365a83160ee1a6d147
mjakmiyo <jedrzejekmonika@gmail.com>
2018-06-19 12:54:11 +0200

    Changed MAX_LEVEL_INK constant to final static

--- src/main/java/edu/iis/powp/controllers/ink/SimmulationInkController.java
+++ src/main/java/edu/iis/powp/controllers/ink/SimmulationInkController.java
@@ -3,73 +3,72 @@ package edu.iis.powp.controllers.ink;
 import java.util.ArrayList;
 
 /**
- * Implementation of Ink Controller for simulation.
- * Singleton design pattern. One unit of ink can be used to draw line of one unit length.
+ * Implementation of Ink Controller for simulation. Singleton design pattern.
+ * One unit of ink can be used to draw line of one unit length.
  */
 public class SimmulationInkController implements InkControllerInterface, Observed {
-    private static SimmulationInkController instance = null;
-    private float inkLevel;
-    private float MAX_LEVEL_INK = 1000;
-    private ArrayList<Observator> observators;
+	private static SimmulationInkController instance = null;
+	private float inkLevel;
+	final static float MAX_LEVEL_INK = 1000;
+	private ArrayList<Observator> observators;
 
-    private SimmulationInkController(){
-        inkLevel = MAX_LEVEL_INK;
-        observators = new ArrayList<>();
-        addObservator(new InkInformer());
-    };
+	private SimmulationInkController() {
+		inkLevel = MAX_LEVEL_INK;
+		observators = new ArrayList<>();
+		addObservator(new InkInformer());
+	};
 
-    /**
-     * Implementation for Singleton design pattern
-     * @return SimmulationInkController instance
-     */
-    public static SimmulationInkController getInstance(){
-        if (instance == null)
-        {
-            instance = new SimmulationInkController();
-        }
-        return instance;
-    }
+	/**
+	 * Implementation for Singleton design pattern
+	 * 
+	 * @return SimmulationInkController instance
+	 */
+	public static SimmulationInkController getInstance() {
+		if (instance == null) {
+			instance = new SimmulationInkController();
+		}
+		return instance;
+	}
 
-    @Override
-    public float getInkLevel() {
-        return inkLevel;
-    }
+	@Override
+	public float getInkLevel() {
+		return inkLevel;
+	}
 
-    @Override
-    public void reduceInkLevel(float level) {
-        inkLevel -= level;
-    }
+	@Override
+	public void reduceInkLevel(float level) {
+		inkLevel -= level;
+	}
 
-    @Override
-    public void fillInk() {
-        inkLevel = MAX_LEVEL_INK;
-    }
+	@Override
+	public void fillInk() {
+		inkLevel = MAX_LEVEL_INK;
+	}
 
-    @Override
-    public boolean checkInkIsEnough(float level) {
-        if (inkLevel >= level) {
-            return true;
-        }
-        else {
-            notifyObservators();
-            return false;
-        }
-    }
+	@Override
+	public boolean checkInkIsEnough(float level) {
+		if (inkLevel >= level) {
+			return true;
+		} else {
+			notifyObservators();
+			return false;
+		}
+	}
 
-    @Override
-    public void addObservator(Observator o) {
-        observators.add(o);
-    }
+	@Override
+	public void addObservator(Observator o) {
+		observators.add(o);
+	}
 
-    @Override
-    public void removeObservator(Observator o) {
-        observators.remove(observators.indexOf(o));
-    }
+	@Override
+	public void removeObservator(Observator o) {
+		observators.remove(observators.indexOf(o));
+	}
 
-    @Override
-    public void notifyObservators() {
-        for(Observator o : observators){
-            o.inform(inkLevel);
-        }
-    }
+	@Override
+	public void notifyObservators() {
+		for (Observator o : observators) {
+			o.inform(inkLevel);
+		}
+	}
 }
